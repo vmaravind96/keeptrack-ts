7ad6143adf5cdaa1308aaf0c724e77ab98f967fa
Aravindakumar V M <aravindakumar_vijayasrimohankumar@intuit.com>
2024-09-20 11:36:26 -0700

    custom hook

--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -53,6 +53,7 @@ function convertToProjectModel(item: any): Project {
 const projectAPI = {
   get(page = 1, limit = 20) {
     return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
+      .then(delay(2000))
       .then(checkStatus)
       .then(parseJSON)
       .then(convertToProjectModels)
